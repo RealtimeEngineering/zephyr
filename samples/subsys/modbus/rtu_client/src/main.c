cafbc371840f34da1eeb3580233af0ace9963f04
SwissKnife64 <christian.waldmann@rte-ag.ch>
2021-05-05 15:00:31 +0200

    modbus samlpes with entries for clion in CMakelist.txt

--- samples/subsys/modbus/rtu_client/src/main.c
+++ samples/subsys/modbus/rtu_client/src/main.c
@@ -16,16 +16,16 @@ static int client_iface;
 
 const static struct modbus_iface_param client_param = {
 	.mode = MODBUS_MODE_RTU,
-	.rx_timeout = 50000,
+	.rx_timeout = 500000,
 	.serial = {
-		.baud = 19200,
-		.parity = UART_CFG_PARITY_NONE,
+		.baud = 115200,
+		.parity = UART_CFG_PARITY_EVEN,
 	},
 };
 
 static int init_modbus_client(void)
 {
-	const char iface_name[] = {DT_PROP(DT_INST(0, zephyr_modbus_serial), label)};
+	const char iface_name[] = {DT_PROP(DT_INST(1, zephyr_modbus_serial), label)};
 
 	client_iface = modbus_iface_get_by_name(iface_name);
 
@@ -35,6 +35,7 @@ static int init_modbus_client(void)
 void main(void)
 {
 	uint16_t holding_reg[8] = {'H', 'e', 'l', 'l', 'o'};
+    uint16_t input_reg[8] = {};
 	const uint8_t coil_qty = 3;
 	uint8_t coil[1] = {0};
 	const int32_t sleep = 250;
@@ -47,22 +48,32 @@ void main(void)
 		return;
 	}
 
-	err = modbus_write_holding_regs(client_iface, node, 0, holding_reg,
-					ARRAY_SIZE(holding_reg));
-	if (err != 0) {
-		LOG_ERR("FC16 failed");
-		return;
-	}
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+	while ( true ) {
+	    for ( int n = 0; n < 7; n++ ) {
+            holding_reg[n] = holding_reg[n+1];
+	    }
+        holding_reg[7] += 1;
+        err = modbus_write_holding_regs(client_iface, node, 0, holding_reg,
+                        ARRAY_SIZE(holding_reg));
+        if (err != 0) {
+            LOG_ERR("FC16 failed");
+            //return;
+        }
+        k_msleep(500);
+        err = modbus_read_holding_regs(client_iface, node, 8, input_reg,
+                           ARRAY_SIZE(input_reg));
+        if (err != 0) {
+            LOG_ERR("FC03 failed with %d", err);
+            //return;
+        }
 
-	err = modbus_read_holding_regs(client_iface, node, 0, holding_reg,
-				       ARRAY_SIZE(holding_reg));
-	if (err != 0) {
-		LOG_ERR("FC03 failed with %d", err);
-		return;
-	}
-
-	LOG_HEXDUMP_INF(holding_reg, sizeof(holding_reg),
-			"WR|RD holding register:");
+        LOG_HEXDUMP_INF(input_reg, sizeof(input_reg),
+                "WR|RD holding register:");
+        k_msleep(500);
+    }
+#pragma clang diagnostic pop
 
 	while (true) {
 		uint16_t addr = 0;
